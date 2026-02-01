d949efa5e3173935dcc9c5566ff25cb0b7818fb1
GianiStatie <gianistati@gmail.com>
2026-02-01 10:58:37 +0200

    fix cast time bug for anivia wall

--- scenes/player/states/cast.gd
+++ scenes/player/states/cast.gd
@@ -1,15 +1,11 @@
 class_name CastState extends State
 
-@onready var timer = $Timer
 var pending_spell_scene: PackedScene = null
-
-
-func _ready() -> void:
-	timer.timeout.connect(_on_pre_cast_timer_ended)
 
 
 func enter(msg := {}) -> void:
 	handle_passed_spell(msg["spell_scene"])
+	owner.velocity = Vector2.ZERO
 	owner.set_can_move(false)
 
 
@@ -18,14 +14,15 @@ func handle_passed_spell(spell_scene):
 	if not inst is Spell:
 		return
 	
-	var anim_length = owner.animation_player.get_animation(self.name).length
-	var speed_scale = anim_length / inst.CAST_DELAY
-	owner.animation_player.speed_scale = speed_scale * 0.5
+	var cast_delay = inst.CAST_DELAY
+	if cast_delay > 0.0:
+		var anim_length = owner.animation_player.get_animation(self.name).length
+		var speed_scale = anim_length / inst.CAST_DELAY
+		owner.animation_player.speed_scale = speed_scale
 	owner.play_animation(self.name)
 	
-	pending_spell_scene = spell_scene
-	timer.start(inst.CAST_DELAY)
 	inst.free()
+	pending_spell_scene = spell_scene
 	owner.update_facing_diection_cast()
 
 
@@ -37,10 +34,10 @@ func _on_pre_cast_timer_ended() -> void:
 	if pending_spell_scene:
 		owner.cast_spell(pending_spell_scene)
 		pending_spell_scene = null
-	owner.set_can_move(true)
-	state_machine.transition_to("Idle")
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == self.name:
-		pass
+		_on_pre_cast_timer_ended()
+		owner.set_can_move(true)
+		state_machine.transition_to("Idle")
